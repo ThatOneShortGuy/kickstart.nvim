c8c24d5b986b71226b617f67ddaea90fdc994377
Braxton Brown <braxton.brown@outlook.com>
2025-07-30 21:51:58 -0600

    add lazygit

--- init.lua
+++ init.lua
@@ -1204,6 +1204,30 @@ require('lazy').setup({
       virtual_text_column = 1, -- virtual text start column, check Start virtual text at column section for more options
     },
   },
+  {
+    'kdheepak/lazygit.nvim',
+    lazy = false,
+    cmd = {
+      'LazyGit',
+      'LazyGitConfig',
+      'LazyGitCurrentFile',
+      'LazyGitFilter',
+      'LazyGitFilterCurrentFile',
+    },
+    -- optional for floating window border decoration
+    dependencies = {
+      'nvim-telescope/telescope.nvim',
+      'nvim-lua/plenary.nvim',
+    },
+    -- setting the keybinding for LazyGit with 'keys' is recommended in
+    -- order to load the plugin when the command is run for the first time
+    keys = {
+      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
+    },
+    config = function()
+      require('telescope').load_extension 'lazygit'
+    end,
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the

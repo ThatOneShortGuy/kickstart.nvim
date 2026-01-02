264b86196a5abddab815e24d4983a57efe992d7f
Braxton Brown <braxton.brown@outlook.com>
2026-01-02 10:30:31 -0700

    allow better copy paste

--- init.lua
+++ init.lua
@@ -280,6 +280,9 @@ if vim.g.neovide then
   vim.keymap.set('n', '<C-->', function()
     change_scale_factor(1 / 1.1)
   end)
+  vim.api.nvim_set_keymap('v', '<sc-c>', '"+y', { noremap = true }) -- Select line(s) in visual mode and copy (CTRL+Shift+V)
+  vim.api.nvim_set_keymap('i', '<sc-v>', '<ESC>"+p', { noremap = true }) -- Paste in insert mode (CTRL+Shift+C)
+  vim.api.nvim_set_keymap('n', '<sc-v>', '"+p', { noremap = true }) -- Paste in normal mode (CTRL+Shift+C)
 end
 
 -- [[ Configure and install plugins ]]

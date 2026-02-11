c86463f8f763d57dac53b3a58ad840acb9179c75
Braxton Brown <braxton.brown@outlook.com>
2026-02-11 08:30:58 -0700

    add mago

--- init.lua
+++ init.lua
@@ -1503,6 +1503,11 @@ require('lazy').setup({
       }
     end,
   },
+  {
+    'calvinludwig/mago.nvim',
+    ft = 'php', -- Load only for PHP files
+    opts = {},
+  },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
